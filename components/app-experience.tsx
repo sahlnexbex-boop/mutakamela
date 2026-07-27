cb8fb8e499a9ec6630559581cea138a6a7a35ab7
sahlnexbex-boop <sahl.nexbex@gmail.com>
2026-07-27 10:49:35 +0530

    app_exp image updated

--- components/app-experience.tsx
+++ components/app-experience.tsx
@@ -41,7 +41,7 @@ export default function AppExperience() {
   ];
 
   return (
-    <section id="app" className="py-16 lg:py-24 bg-[#F0EFFD] relative overflow-hidden">
+    <section id="app" className="py-16 lg:py-24 bg-[#eeebfc] relative overflow-hidden">
       <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 xl:px-12">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 items-center">
 
